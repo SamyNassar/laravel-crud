40da8ea430f09e296887b98fdd8bbb010558968b
Samy Nassar <samynassar996@gmail.com>
2023-07-04 15:43:53 +0300

    Implement app router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class, 'index']);
+Route::get('create', [TodoController::class, 'create']);
+Route::get('details/{todo}', [TodoController::class, 'details']);
+Route::get('edit/{todo}', [TodoController::class, 'edit']);
+Route::post('update/{todo}', [TodoController::class, 'update']);
+Route::post('store-data', [TodoController::class, 'store']);
+Route::get('delete/{todo}', [TodoController::class, 'delete']);
